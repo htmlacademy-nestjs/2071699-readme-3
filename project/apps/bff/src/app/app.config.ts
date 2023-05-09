8d4d9aa95f77e77aca3f04ac8832bbc583a4f1b7
GSotskova <lgv11.09@gmail.com>
2023-05-09 16:58:49 +0300

    Загрузка аватара и изображений для публикации BFF

--- project/apps/bff/src/app/app.config.ts
+++ project/apps/bff/src/app/app.config.ts
@@ -4,7 +4,8 @@ export enum ApplicationServiceURL {
   Post = 'http://localhost:3334/api/post',
   Posts = 'http://localhost:3334/api/posts',
   Likes = 'http://localhost:3335/api/like',
-  Comments = 'http://localhost:3335/api/comment'
+  Comments = 'http://localhost:3335/api/comment',
+  Uploads = 'http://localhost:3337/api/files/upload'
 }
 
 export const HTTP_CLIENT_MAX_REDIRECTS = 5;
